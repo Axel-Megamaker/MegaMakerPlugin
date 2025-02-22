e41046437d85de673db63784f803039cbd3dfa0a
Bronken4 <axel.svensson@megamaker.se>
2025-02-22 20:02:26 +0100

    First commit

---
+++ MegaMaker.vue
@@ -0,0 +1,53 @@
+<style>
+
+</style>
+
+<template>
+    <v-container class="flex">
+        <v-row>
+            <v-col cols="4">
+                <v-sheet
+                    height="300"
+                    >
+                    <v-row>
+                        <p class="mx-5 text-h6 font-weight-black">Sensors</p>
+                    </v-row>
+                    
+                    <v-row>
+                        <v-col class="ml-2" cols="6">
+                            <p>Sensor_1</p>
+                        </v-col>
+                        <v-col>
+                            <v-chip color="green">
+                                Success
+                            </v-chip>
+                        </v-col>
+                    </v-row>
+
+                    <v-row>
+                        <v-col class="ml-2" cols="6">
+                            <p>Sensor_2</p>
+                        </v-col>
+                        <v-col>
+                            <v-chip color="red">
+                                Error
+                            </v-chip>
+                        </v-col>
+                    </v-row>
+
+                </v-sheet>  
+            </v-col>
+        </v-row>
+        
+    </v-container>
+    
+
+</template>
+
+<script lang="js">
+import Vue from "vue";
+
+export default Vue.extend({
+	
+});
+</script>
---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+import { injectComponent } from "..";
+import { registerRoute } from '@/routes';
+
+import MegaMaker from "./MegaMaker.vue"
+
+// Add menu named MegaMaker to Control. Change "Control" to , Job, Files or Settings to change.
+registerRoute(MegaMaker, {
+    Control: {
+        MegaMaker: {
+            icon: 'mdi-size-m',
+            caption: 'MegaMaker',
+            path: '/Plugins/MegaMaker',
+        },
+    },
+});
