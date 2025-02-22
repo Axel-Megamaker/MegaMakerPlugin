d3b6acfa3359a34611693111d45dd8779bd5e24d
Bronken4 <axel.svensson@megamaker.se>
2025-02-23 00:04:46 +0100

    Added loggerComponent

---
+++ LoggerComponent.vue
@@ -0,0 +1,18 @@
+<template>
+        <v-row>
+            <v-sheet>
+                <v-row>
+                    <p class="ma-5 text-h6 font-weight-black">Logger</p>
+                </v-row>
+
+                <v-row>
+                    <v-col>
+                        <v-btn class="ma-5" color="primary">Generate System Log</v-btn>
+                    </v-col>
+                </v-row>
+
+
+                
+            </v-sheet>
+        </v-row>
+</template>
