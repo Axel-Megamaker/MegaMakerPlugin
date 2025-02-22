f3e118ad22f19333d2e3864e02024a69a29ce053
Bronken4 <axel.svensson@megamaker.se>
2025-02-23 00:06:00 +0100

    Updated imports

--- index.js
+++ index.js
@@ -2,15 +2,16 @@
 
 import { registerRoute } from '@/routes';
 
-import MegaMaker from "./MegaMaker.vue"
+import MainContent from "./MainContent.vue"
+import themeConfig from "./themeConfig.js"
 
 // Add menu named MegaMaker to Control. Change "Control" to , Job, Files or Settings to change.
-registerRoute(MegaMaker, {
+registerRoute(MainContent, {
     Control: {
         MegaMaker: {
             icon: 'mdi-size-m',
             caption: 'MegaMaker',
-            path: '/Plugins/MegaMaker'
+            path: '/Plugins/MegaMakerPlugin'
         },
     },
 });
