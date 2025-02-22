deadad71131f68237e35ef4ea326267e43b5359f
Bronken4 <axel.svensson@megamaker.se>
2025-02-23 00:05:40 +0100

    Setup the file system

---
+++ MainContent.vue
@@ -0,0 +1,33 @@
+<style>
+
+</style>
+
+<template>
+    <v-container class="flex">
+        <v-row>
+            <v-col col="8">
+                <FilterComponent/>
+            </v-col>
+            <v-col col="4">
+                <LoggerComponent/>
+            </v-col>
+        </v-row>
+        
+        
+    </v-container>
+    
+</template>
+
+<script lang="js">
+import Vue from "vue";
+import FilterComponent from "./FilterComponent.vue";
+import LoggerComponent from "./LoggerComponent.vue";
+
+export default Vue.extend({
+    components: {
+        FilterComponent,
+        LoggerComponent
+    }
+	
+});
+</script>
